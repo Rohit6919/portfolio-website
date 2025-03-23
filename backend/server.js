ce2a7005e3bb2babfbe24b101c867a065bd347b4
Rohit patel <patelrohit6919@gmail.com>
2025-03-23 23:00:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,5 +27,4 @@ mongoose.connect(process.env.MONGO_URI, {
 // Routes
 app.use('/api/contact', contactRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
